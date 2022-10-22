8f4951c18e0a6d0bd924c20694f58dcb48b850f3
anowarzz <anowarhosen444@gmail.com>
2022-10-22 12:10:44 +0600

    id wise data processed

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(cors())
 
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+
 
 
 app.get('/', (req, res) => {
@@ -18,6 +21,31 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
 
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if( id === 08){
+        res.send(news)
+    }
+    else{
+        const category_news = news.filter( n => n.category_id === id) 
+        res.send(category_news)
+    }
+
+  
+})
+
+
+app.get('/news/:id', (req, res) => {
+    const id  = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+
+
+
+// Server running test
 app.listen(port, () => {
     console.log(`Server running On ${port}`)
 })
